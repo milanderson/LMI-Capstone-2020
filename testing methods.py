ec11f4ccf184eebbe7cc4c4ae73627cb9ed05354
Summer Chambers <ssc4mc@virginia.edu>
2020-10-15 18:36:34 -0400

    added initial functions

---
+++ testing methods.py
@@ -0,0 +1,18 @@
+'''
+Testing Methods
+'''
+
+def getComments():
+    pass
+
+
+def getCommentRules(): # read csv
+    pass 
+
+
+def queryComments(index, comments):
+    pass
+
+
+def scoreResults(true, pred):
+    pass
