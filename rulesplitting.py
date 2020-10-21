a8812be8d42628e6d3f5633179307e0e1f7134cb
Summer Chambers <ssc4mc@virginia.edu>
2020-10-21 18:34:02 -0400

    updating rulesplitting methods

--- rulesplitting.py
+++ rulesplitting.py
@@ -7,11 +7,20 @@ es = Elasticsearch() #put the aws url in here
 
 def splitRule():
     ...
-    return rulechunks
+    #return rulechunks
 
 
-def ruletoES(rulechunks, i):
+def ruletoES_lists(rulechunks, i):
     for chunk in rulechunks:
-        res = es.index(index=chunk["name"], id=i, body=chunk["text"])
+        res = es.index(index=chunk.key, id=i, body=chunk.value)
         i += 1
     return i
+
+
+def ruletoES_dicts(rulechunks, i):
+    for chunk in rulechunks:
+        for j, (key, value) in enumerate(chunk.items()):
+            res = es.index(index=chunk.key+key, id=i+j, body=value)
+        i += 1
+    return i
+    
