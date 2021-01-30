300cc1f7b9555694598f361bd344461a1007718b
milanderson <43865089+milanderson@users.noreply.github.com>
2021-01-30 01:46:16 -0500

    Update skos2solr.py
    Switching to partial entailment to support lemmatization. Fixing whitespace splitting issue.

--- Examples/CMS/Utils/skos2solr.py
+++ Examples/CMS/Utils/skos2solr.py
@@ -3,12 +3,14 @@ import xml.etree.ElementTree as ET
 
 USAGE = '''
     Usage:
-        skos2solr.py input [savepath]
+        skos2solr.py input mode [savepath]
     Description:
         converts a SKOS dictionary into a set of SOLR synonym rules
     Arguments:
         input
             An filepath or XML formatted string
+        mode
+            One of "b" / "broader", "n" / "narrower", or "e" / "explosive". Sets the entailment direction to move up, down, or both directions along the SKOS graph.
         savepath (Optional)
             The filepath to save results to
 '''
@@ -21,11 +23,12 @@ class skos2solr():
     """
         Converts SKOS dictionaries into a SOLR synonym list
     """
+    MODES = ["b", "broader", "n", "narrower", "e", "explosive"]
 
     def __init__(self):
         pass
 
-    def buildSOLR(self, concepts):
+    def buildSOLR(self, concepts, mode):
         """
             Convert a list of SKOS concepts into SOLR synonym rule text
 
@@ -37,9 +40,37 @@ class skos2solr():
         """
         rules = ""
         for c in concepts:
+            # since Lucene synonyms are processed greedily, we must sort to prevent chain entailment
+            if mode == "n" or mode == "narrower":
+                conceptDict = {l:c for c in concepts for l in c.labels}
+                sortedConcepts = [c for c in concepts if not c.narrower]
+                i = 0
+                while i < len(sortedConcepts):
+                    for label in sortedConcepts[i].broader:
+                        if label in conceptDict and conceptDict[label] not in sortedConcepts:
+                            sortedConcepts.append(conceptDict[label])
+                    i +=1
+                concepts = sortedConcepts
+                concepts.reverse()
+            if mode == "b" or mode == "broader":
+                conceptDict = {l:c for c in concepts for l in c.labels}
+                sortedConcepts = [c for c in concepts if not c.broader]
+                i = 0
+                while i < len(sortedConcepts):
+                    for label in sortedConcepts[i].narrower:
+                        if label in conceptDict and conceptDict[label] not in sortedConcepts:
+                            sortedConcepts.append(conceptDict[label])
+                    i +=1
+                concepts = sortedConcepts
+                concepts.reverse()
+
             try:
-                reflectives = [lab + app for lab in c.labels for app in '12']
-                rules += ", ".join(c.labels) + " => " + ", ".join(reflectives + c.synonyms + c.broader + c.narrower) + '\n'
+                if mode == "e" or mode == "explosive":
+                    rules += ", ".join(c.labels) + " => " + ", ".join(c.labels + c.synonyms + c.broader + c.narrower) + '\n'
+                if mode == "n" or mode == "narrower":
+                    rules += ", ".join(c.labels) + " => " + ", ".join(c.labels + c.synonyms + c.narrower) + '\n'
+                if mode == "b" or mode == "broader":
+                    rules += ", ".join(c.labels) + " => " + ", ".join(c.labels + c.synonyms + c.broader) + '\n'
             except Exception as e:
                 print(c.XMLNode.tag, c.XMLNode.attrib)
                 raise e
@@ -70,7 +101,7 @@ class skos2solr():
         concepts = [Concept(node) for node in root.getchildren()]
         return concepts
 
-    def __call__(self, dataSource, outPath='synonyms.solr'):
+    def __call__(self, mode, dataSource, outPath='synonyms.solr'):
         """
             Convert a a SKOS XML file or string into SOLR synonym rule text and save to file
 
@@ -78,8 +109,11 @@ class skos2solr():
                 dataSource: a SKOS XML file or string
                 outPath: OPTIONAL. A file path. Defaults to 'synonyms.solr'
         """
+        if not any(mode == m for m in skos2solr.MODES):
+            raise ValueError("Mode must be one of: " + " ".join(skos2solr.MODES))
+
         concepts = self.parseSKOS(dataSource)
-        rules = self.buildSOLR(concepts)
+        rules = self.buildSOLR(concepts, mode)
         f = open(outPath, 'w')
         f.write(rules)
         f.close()
@@ -98,33 +132,37 @@ class Concept():
         XMLproperties = XMLNode.getchildren()
         for p in XMLproperties:
             propName = p.tag[p.tag.rfind('}') + 1:]
-            print(propName)
             if propName == 'prefLabel':
                 self.labels = Concept._split(p.text)
+            elif propName == 'acronym':
+                self.labels += Concept._split(p.text)
+            elif propName == 'altLabel':
+                self.labels += Concept._split(p.text)
             elif propName == 'broader':
-                self.broader += Concept._split(p.text, '1')
+                self.broader += Concept._split(p.text)
             elif propName == 'narrower':
-                self.narrower += Concept._split(p.text, '2')
+                self.narrower += Concept._split(p.text)
             elif propName == 'synonym':
-                self.synonyms += Concept._split(p.text, '1')
-                self.synonyms += Concept._split(p.text, '2')
+                self.synonyms += Concept._split(p.text)
 
     @staticmethod
-    def _split(text, appendor=''):
-        words = [word + appendor for word in re.split('; | (| )|, ', text) if word]
-        extra = []
-        for w in words:
+    def _split(text):
+        def replaceParen(w):
             if '(' in w and w[w.find('(') + 1:w.find('(') + 2].lower() in 'qwertyuiopasdfghjklzxcvbnm':
-                extra.append(w.replace('(','').replace(')',''))
-        return words + extra
+                return w.replace('(','').replace(')','')
+            elif w[-1] == ")" and "(" not in w:
+                return w[:-1]
+            else:
+                return w
+        return [replaceParen(word) for word in re.split('; | \(|\) |, ', text)]
 
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) < 2 or len(sys.argv) > 3:
+    if len(sys.argv) < 3 or len(sys.argv) > 4:
         print(USAGE)
     else:
         converter = skos2solr()
-        if len(sys.argv) == 3:
-            converter(sys.argv[1], sys.argv[2])
+        if len(sys.argv) == 4:
+            converter(sys.argv[1], sys.argv[2], sys.argv[3])
         else:
-            converter(sys.argv[1])
+            converter(sys.argv[1], sys.argv[2])
